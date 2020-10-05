dde9ff2869135e82b71956d3ea5dd1d788a02fb1
Jan <malinowwskijasiek1999@gmail.com>
2020-10-05 21:43:43 +0200

    Index

--- image_inspector/inspector/inspector.py
+++ image_inspector/inspector/inspector.py
@@ -71,8 +71,8 @@ class ImageInspector:
 
         # writing the new values into a dataframe
 
-        self.result.loc[self.iterator, :] = values
-        self.resilt.loc[self.iterator, 'Image Name'] = self.imgs[self.iterator]
+        self.result.loc[self.iterator, :-1] = values
+        self.result.loc[self.iterator, 'Image Name'] = self.imgs[self.iterator]
 
     # Callback subtracting 1 from the iterator, loading a previous image and
     # results for the previous image
@@ -86,5 +86,6 @@ class ImageInspector:
     def get_results(self):
         if self.iterator == len(self.imgs) - 1 or self.iterator == 0:
             values = self.grid.get_values()
-            self.result.loc[self.iterator, :] = values
+            self.result.loc[self.iterator, :-1] = values
+            self.result.loc[self.iterator, 'Image Name'] = self.imgs[self.iterator]
         return self.result
